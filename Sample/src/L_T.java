626c8637baa295a74ec2389341f4e51cbd22a9e8
gurudileep1997 <dileepguru7@gmail.com>
2021-12-01 17:12:48 +0530

    new added

--- Sample/src/L_T.java
+++ Sample/src/L_T.java
@@ -3,5 +3,7 @@ public class L_T {
 public static void main(String[] args) {
 	System.out.println("Sreenadh");
 	System.out.println("Hafseen");
+	System.out.println("Gurappa");
+	System.out.println("Friends");
 }
 }
